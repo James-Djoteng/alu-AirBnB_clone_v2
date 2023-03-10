1ff3428bbebbb7faa4a8994d9e3838bc9f5fea5d
Pro <pro@Macbook-pro-de-pro.local>
2023-03-10 14:08:09 +0200

    e

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,43 +1,67 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
+The 2-do_deploy_web_static.pyy module
+Fabric script (based on the file 1-pack_web_static.py) that
+distributes an archive to your web servers, using the function do_deploy
 """
-
-from datetime import datetime
-from fabric.api import *
-import shlex
-import os
+from os.path import isfile
+from fabric.api import put, run, env, local
+env.hosts = ['35.227.98.159', '104.196.65.233']
 
 
-env.hosts = ['54.173.201.66', '54.221.144.230']
-env.user = 'ubuntu'
+def do_pack():
+    """
+    generate a .tgz archive from web_static with do_pack
+    """
+    now = datetime.datetime.now()
+    archive = 'web_static_' + str(now.year) + str(now.month) +\
+        str(now.day) + str(now.hour) + str(now.minute) +\
+        str(now.second) + '.tgz'
+
+    if os.path.isdir("versions") is False:
+        local('mkdir -p versions')
+
+    local("tar -cvzf versions/{} web_static".format(archive))
+    if (isfile('version/{}'.format(archive))):
+        return ('version/{}'.format(archive))
+    else:
+        return None
 
 
 def do_deploy(archive_path):
-    """ Deploys """
-    if not os.path.exists(archive_path):
+    """distributes an archive to web servers using do_deploy func
+    """
+    if (isfile(archive_path)) is False:
         return False
-    try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
 
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
-        return True
-    except:
+    noex = archive_path.split('/')[1].split('.')[0]
+    ex = archive_path.split('/')[1]
+    re = put(archive_path, "/tmp/")
+    if re.failed:
         return False
+    re = run('mkdir -p /data/web_static/releases/{}/'.format(noex))
+    if re.failed:
+        return False
+    re = run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+        ex, noex))
+    if re.failed:
+        return False
+    re = run('rm /tmp/{}.tgz'.format(noex))
+    if re.failed:
+        return False
+    re = run('mv /data/web_static/releases/{}/web_static/* \
+/data/web_static/releases/{}/'.format(noex, noex))
+    if re.failed:
+        return False
+    re = run('rm -rf /data/web_static/releases/{}/web_static'.format(noex))
+    if re.failed:
+        return False
+    re = run('rm -rf /data/web_static/current')
+    if re.failed:
+        return False
+    re = run('ln -s /data/web_static/releases/{}/\
+            /data/web_static/current'.format(noex))
+    if re.failed:
+        return False
+    print("New version deployed")
+    return True
