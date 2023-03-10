151107492f1d89434e9058de04eb522c1eaf458e
Pro <pro@Macbook-pro-de-pro.local>
2023-03-10 14:12:07 +0200

    e

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,67 +1,80 @@
 #!/usr/bin/python3
 """
-The 2-do_deploy_web_static.pyy module
-Fabric script (based on the file 1-pack_web_static.py) that
-distributes an archive to your web servers, using the function do_deploy
+Fabric script that distributes an archive to web servers
 """
-from os.path import isfile
-from fabric.api import put, run, env, local
-env.hosts = ['35.227.98.159', '104.196.65.233']
+import os.path
+from fabric.api import env, local, put, run
+from datetime import datetime
+
+env.user = 'ubuntu'
+env.hosts = ['3.90.0.75', '54.175.29.140']
 
 
 def do_pack():
     """
-    generate a .tgz archive from web_static with do_pack
+    Create a tar gzipped archive of the directory web_static.
     """
-    now = datetime.datetime.now()
-    archive = 'web_static_' + str(now.year) + str(now.month) +\
-        str(now.day) + str(now.hour) + str(now.minute) +\
-        str(now.second) + '.tgz'
-
-    if os.path.isdir("versions") is False:
-        local('mkdir -p versions')
-
-    local("tar -cvzf versions/{} web_static".format(archive))
-    if (isfile('version/{}'.format(archive))):
-        return ('version/{}'.format(archive))
-    else:
+    try:
+        if not os.path.isdir("versions"):
+            os.makedirs("versions")
+        now = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(now)
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except:
         return None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to web servers using do_deploy func
     """
-    if (isfile(archive_path)) is False:
+    Distributes an archive to web servers
+    """
+    if not os.path.exists(archive_path):
         return False
 
-    noex = archive_path.split('/')[1].split('.')[0]
-    ex = archive_path.split('/')[1]
-    re = put(archive_path, "/tmp/")
-    if re.failed:
+    try:
+        archive_name = os.path.basename(archive_path)
+        no_ext = os.path.splitext(archive_name)[0]
+
+        # Upload archive to /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+
+        # Create directory to uncompress the archive
+        run("sudo mkdir -p /data/web_static/releases/{}".format(no_ext))
+
+        # Uncompress archive into the folder created
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(archive_name, no_ext))
+
+        # Delete archive from web server
+        run("sudo rm /tmp/{}".format(archive_name))
+
+        # Move uncompressed files to its own folder
+        run("sudo mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/"
+            .format(no_ext, no_ext))
+
+        # Remove original web_static folder
+        run("sudo rm -rf /data/web_static/releases/{}/web_static"
+            .format(no_ext))
+
+        # Delete symbolic link
+        run("sudo rm -rf /data/web_static/current")
+
+        # Create new symbolic link
+        run("sudo ln -s /data/web_static/releases/{}/ "
+            "/data/web_static/current".format(no_ext))
+
+        # Add a new file to the deployment
+        run("sudo touch /data/web_static/releases/{}/index.html"
+            .format(no_ext))
+        run("sudo echo '<html><head></head><body>Holberton School</body></html>' "
+            "| sudo tee /data/web_static/releases/{}/index.html"
+            .format(no_ext))
+
+        print("New version deployed!")
+        return True
+
+    except Exception as e:
+        print(e)
         return False
-    re = run('mkdir -p /data/web_static/releases/{}/'.format(noex))
-    if re.failed:
-        return False
-    re = run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
-        ex, noex))
-    if re.failed:
-        return False
-    re = run('rm /tmp/{}.tgz'.format(noex))
-    if re.failed:
-        return False
-    re = run('mv /data/web_static/releases/{}/web_static/* \
-/data/web_static/releases/{}/'.format(noex, noex))
-    if re.failed:
-        return False
-    re = run('rm -rf /data/web_static/releases/{}/web_static'.format(noex))
-    if re.failed:
-        return False
-    re = run('rm -rf /data/web_static/current')
-    if re.failed:
-        return False
-    re = run('ln -s /data/web_static/releases/{}/\
-            /data/web_static/current'.format(noex))
-    if re.failed:
-        return False
-    print("New version deployed")
-    return True
